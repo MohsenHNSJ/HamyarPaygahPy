c8945221f5898ffedfcd52dc543f2b9bf9d1c512
MohsenHNSJ <97420292+MohsenHNSJ@users.noreply.github.com>
2025-12-30 20:02:42 +0000

    Update license info

--- src/HamyarPaygahPy/__init__.py
+++ src/HamyarPaygahPy/__init__.py
@@ -4,13 +4,13 @@
 A Package for EMS overseers to gather information from EMS servers.
 
 :copyright: (c) 2026-present MohsenHNSJ
-:license: GPL-3.0, see LICENSE for more details
+:license: MIT, see LICENSE for more details
 
 """  # noqa: D205
 
 __title__ = "HamyarPaygahPy"
 __author__ = "MohsenHNSJ"
-__license__ = "GPL-3.0"
+__license__ = "MIT"
 __copyright__ = "Copyright 2026-present MohsenHNSJ"
 __version__ = "0.1.0"
 
