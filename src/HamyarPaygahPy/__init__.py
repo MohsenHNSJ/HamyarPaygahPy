08813081f8289684f19b87a710334dc0c2bad23d
MohsenHNSJ <97420292+MohsenHNSJ@users.noreply.github.com>
2025-12-30 18:04:12 +0000

    Init file added

---
+++ src/HamyarPaygahPy/__init__.py
@@ -0,0 +1,35 @@
+"""Hamyar Paygah.
+--------------------------
+
+A Package for EMS overseers to gather information from EMS servers.
+
+:copyright: (c) 2026-present MohsenHNSJ
+:license: GPL-3.0, see LICENSE for more details
+
+"""  # noqa: D205
+
+__title__ = "HamyarPaygahPy"
+__author__ = "MohsenHNSJ"
+__license__ = "GPL-3.0"
+__copyright__ = "Copyright 2026-present MohsenHNSJ"
+__version__ = "0.1.0"
+
+# from . import constants, enums, exceptions, models, utils
+# from .authorization import AuthorizationClass
+# from .base import BaseClass
+# from .margin import MarginClass
+# from .order import OrderClass
+# from .wallet import WalletClass
+
+# __all__: list[str] = [
+#     "AuthorizationClass",
+#     "BaseClass",
+#     "MarginClass",
+#     "OrderClass",
+#     "WalletClass",
+#     "constants",
+#     "enums",
+#     "exceptions",
+#     "models",
+#     "utils",
+# ]
