fb26b3139b52dee70d56bb6f1e4127fba3816e4b
Mohsen <97420292+MohsenHNSJ@users.noreply.github.com>
2025-12-31 10:23:25 +0000

    Fix naming issue

---
+++ src/hamyar_paygah_py/__init__.py
@@ -0,0 +1,15 @@
+"""Hamyar Paygah.
+--------------------------
+
+A Package for EMS overseers to gather information from EMS servers.
+
+:copyright: (c) 2026-present MohsenHNSJ
+:license: MIT, see LICENSE for more details
+
+"""  # noqa: D205
+
+__title__ = "HamyarPaygahPy"
+__author__ = "MohsenHNSJ"
+__license__ = "MIT"
+__copyright__ = "Copyright 2026-present MohsenHNSJ"
+__version__ = "0.1.0"
