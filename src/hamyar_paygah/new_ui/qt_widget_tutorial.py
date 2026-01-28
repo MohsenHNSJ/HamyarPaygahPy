51dcfff59b4f534ef55ee408df0aebbd0c0cdb5b
Mohsen <97420292+MohsenHNSJ@users.noreply.github.com>
2026-01-28 08:55:22 +0000

    more training

--- src/hamyar_paygah/new_ui/qt_widget_tutorial.py
+++ src/hamyar_paygah/new_ui/qt_widget_tutorial.py
@@ -1,7 +1,14 @@
 """Simple Qt application that creates and shows a widget."""
 # pylint: disable=E0611,R0903
 
-from PySide6.QtWidgets import QApplication, QMainWindow, QPushButton
+from PySide6.QtWidgets import (
+    QApplication,
+    QLabel,
+    QLineEdit,
+    QMainWindow,
+    QVBoxLayout,
+    QWidget,
+)
 
 
 # Subclass QMainWindow to customize your application's main window.
@@ -15,22 +22,20 @@ class MainWindow(QMainWindow):
         # Set title
         self.setWindowTitle("My App")
 
-        # Create a button in the window
-        self.button = QPushButton("Press Me")
-        self.button.clicked.connect(self.the_button_was_clicked)
+        self.label = QLabel()
 
-        # Set the window to a fixed size
-        self.setFixedSize(400, 300)
+        self.input = QLineEdit()
+        self.input.textChanged.connect(self.label.setText)
+
+        layout = QVBoxLayout()
+        layout.addWidget(self.input)
+        layout.addWidget(self.label)
+
+        container = QWidget()
+        container.setLayout(layout)
 
         # Set the central widget of the Window.
-        self.setCentralWidget(self.button)
-
-    def the_button_was_clicked(self) -> None:
-        """Handle button click event."""
-        self.button.setText("You already pressed me!")
-        self.button.setEnabled(False)
-
-        self.setWindowTitle("My App (Button Pressed)")
+        self.setCentralWidget(container)
 
 
 # You need one (and only one) QApplication instance per application.
