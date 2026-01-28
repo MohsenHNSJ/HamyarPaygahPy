166f54e39f13614c96ad132500b1a04664ffc1db
Mohsen <97420292+MohsenHNSJ@users.noreply.github.com>
2026-01-28 06:56:43 +0000

    Added pyside stubs file to project development requirements

---
+++ src/hamyar_paygah/new_ui/qt_widget_tutorial.py
@@ -0,0 +1,17 @@
+"""Simple Qt application that creates and shows a widget."""
+
+from PySide6.QtWidgets import QApplication, QWidget
+
+# You need one (and only one) QApplication instance per application.
+# Pass in sys.argv to allow command line arguments for your app.
+# If you know you won't use command line arguments QApplication([]) works too.
+app = QApplication([])
+
+# Create a Qt widget, which will be our main window.
+window = QWidget()
+window.show()  # IMPORTANT!!!!! Windows are hidden by default.
+
+# Start the event loop.
+app.exec()
+
+# Your application won't reach here until you exit and the event loop has stopped.
