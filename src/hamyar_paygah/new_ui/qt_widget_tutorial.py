eddba895ffff911746dd16db289bf7fe576bdb2f
Mohsen <97420292+MohsenHNSJ@users.noreply.github.com>
2026-01-28 08:08:18 +0000

    Some more training

--- src/hamyar_paygah/new_ui/qt_widget_tutorial.py
+++ src/hamyar_paygah/new_ui/qt_widget_tutorial.py
@@ -12,12 +12,31 @@ class MainWindow(QMainWindow):
         """Main window constructor."""
         super().__init__()
 
+        # A variable to store the state of the button
+        self.button_is_checked = True
+
+        # Set title
         self.setWindowTitle("My App")
 
+        # Create a button in the window
         button = QPushButton("Press Me")
+        button.setCheckable(True)
+        button.clicked.connect(self.the_button_was_toggled)
+
+        # Set the window to a fixed size
+        self.setFixedSize(400, 300)
 
         # Set the central widget of the Window.
         self.setCentralWidget(button)
+
+    def the_button_was_clicked(self) -> None:
+        """Handle button click event."""
+        print("You clicked the button!")
+
+    def the_button_was_toggled(self, checked: bool) -> None:  # noqa: FBT001
+        """Handle button toggle event."""
+        self.button_is_checked = checked
+        print("Checked?", self.button_is_checked)
 
 
 # You need one (and only one) QApplication instance per application.
