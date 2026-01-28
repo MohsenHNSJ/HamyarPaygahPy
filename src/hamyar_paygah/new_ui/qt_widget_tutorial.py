55eada25ef9b7d72499119405b3041201e439356
Mohsen <97420292+MohsenHNSJ@users.noreply.github.com>
2026-01-28 07:47:25 +0000

    Initial window with a button

--- src/hamyar_paygah/new_ui/qt_widget_tutorial.py
+++ src/hamyar_paygah/new_ui/qt_widget_tutorial.py
@@ -1,7 +1,24 @@
 """Simple Qt application that creates and shows a widget."""
-# pylint: disable=E0611
+# pylint: disable=E0611,R0903
 
-from PySide6.QtWidgets import QApplication, QWidget
+from PySide6.QtWidgets import QApplication, QMainWindow, QPushButton
+
+
+# Subclass QMainWindow to customize your application's main window.
+class MainWindow(QMainWindow):
+    """Application main window."""
+
+    def __init__(self) -> None:
+        """Main window constructor."""
+        super().__init__()
+
+        self.setWindowTitle("My App")
+
+        button = QPushButton("Press Me")
+
+        # Set the central widget of the Window.
+        self.setCentralWidget(button)
+
 
 # You need one (and only one) QApplication instance per application.
 # Pass in sys.argv to allow command line arguments for your app.
@@ -9,7 +26,7 @@ from PySide6.QtWidgets import QApplication, QWidget
 app = QApplication([])
 
 # Create a Qt widget, which will be our main window.
-window = QWidget()
+window = MainWindow()
 window.show()  # IMPORTANT!!!!! Windows are hidden by default.
 
 # Start the event loop.
