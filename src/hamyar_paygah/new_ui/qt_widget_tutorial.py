4eee3497c032c18e9eee4b008e28918bf0ce55b4
Mohsen <97420292+MohsenHNSJ@users.noreply.github.com>
2026-01-29 12:49:49 +0000

    More training

--- src/hamyar_paygah/new_ui/qt_widget_tutorial.py
+++ src/hamyar_paygah/new_ui/qt_widget_tutorial.py
@@ -1,14 +1,9 @@
 """Simple Qt application that creates and shows a widget."""
 
 # pylint: disable=E0611,R0903
-from PySide6.QtCore import Qt
-from PySide6.QtGui import QAction
 from PySide6.QtWidgets import (
     QApplication,
-    QLabel,
     QMainWindow,
-    QStatusBar,
-    QToolBar,
 )
 
 
@@ -21,26 +16,6 @@ class MainWindow(QMainWindow):
         super().__init__()
 
         self.setWindowTitle("My App")
-
-        label = QLabel("Hello!")
-        label.setAlignment(Qt.AlignmentFlag.AlignCenter)
-
-        self.setCentralWidget(label)
-
-        toolbar = QToolBar("My main toolbar")
-        self.addToolBar(toolbar)
-
-        button_action = QAction("Your button", self)
-        button_action.setStatusTip("This is your button")
-        button_action.triggered.connect(self.toolbar_button_clicked)
-        button_action.setCheckable(True)
-        toolbar.addAction(button_action)
-
-        self.setStatusBar(QStatusBar(self))
-
-    def toolbar_button_clicked(self, state: bool) -> None:  # noqa: FBT001
-        """Runs the function related to toolbar button click."""
-        print("click", state)
 
 
 # You need one (and only one) QApplication instance per application.
