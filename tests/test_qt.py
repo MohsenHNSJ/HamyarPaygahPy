9ca310fa2ce90ae623050370e06daa7d9456e83f
Mohsen <97420292+MohsenHNSJ@users.noreply.github.com>
2026-02-02 14:44:32 +0000

    Slots test

--- tests/test_qt.py
+++ tests/test_qt.py
@@ -1,10 +1,25 @@
 """Test for qt."""
 
 # pylint: disable=C0114,I1101,E0611
-from PySide6.QtWidgets import QApplication, QLabel
+from PySide6.QtCore import Slot
+from PySide6.QtWidgets import QApplication, QPushButton
 
+
+@Slot()
+def say_hello() -> None:
+    """Prints Hello to console."""
+    print("Button clicked, Hello!")
+
+
+# Create the Qt application
 app = QApplication([])
-# This HTML approach will be valid too!
-label = QLabel("<font color=red size=40>Hello World!</font>")
-label.show()
+
+# Create a button
+button = QPushButton("Click me")
+# Connect the button to the function
+button.clicked.connect(say_hello)
+# Show the button
+button.show()
+
+# Run the main Qt loop
 app.exec()
