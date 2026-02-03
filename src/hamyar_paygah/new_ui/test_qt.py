c20eeccc6c0f90146b3130c3bd4871846cfe5d02
Mohsen <97420292+MohsenHNSJ@users.noreply.github.com>
2026-02-03 06:48:59 +0000

    Some training on qt

--- src/hamyar_paygah/new_ui/test_qt.py
+++ src/hamyar_paygah/new_ui/test_qt.py
@@ -1,25 +1,44 @@
 """Test for qt."""
 
-# pylint: disable=C0114,I1101,E0611
-from PySide6.QtCore import Slot
-from PySide6.QtWidgets import QApplication, QPushButton
+# pylint: disable=C0114,I1101,E0611,R0903
+import sys
+
+from PySide6.QtWidgets import QApplication, QDialog, QLineEdit, QPushButton, QVBoxLayout
 
 
-@Slot()
-def say_hello() -> None:
-    """Prints Hello to console."""
-    print("Button clicked, Hello!")
+class Form(QDialog):
+    """Main window class."""
+
+    def __init__(self) -> None:
+        """Window initializer."""
+        super().__init__()
+        self.setWindowTitle("My Form")
+
+        # Create widgets
+        self.edit = QLineEdit("Write my name here...")
+        self.button = QPushButton("Show Greetings")
+
+        # Create layout and add widgets
+        layout = QVBoxLayout()
+        layout.addWidget(self.edit)
+        layout.addWidget(self.button)
+
+        # Set dialog layout
+        self.setLayout(layout)
+
+        # Add button signal to greetings slot
+        self.button.clicked.connect(self.greetings)
+
+    def greetings(self) -> None:
+        """Shows a greeting message."""
+        print("Hello " + self.edit.text())
 
 
-# Create the Qt application
-app = QApplication([])
-
-# Create a button
-button = QPushButton("Click me")
-# Connect the button to the function
-button.clicked.connect(say_hello)
-# Show the button
-button.show()
-
-# Run the main Qt loop
-app.exec()
+if __name__ == "__main__":
+    # Create the Qt Application
+    app = QApplication(sys.argv)
+    # Create and show the form
+    form = Form()
+    form.show()
+    # Run the main Qt loop
+    sys.exit(app.exec())
