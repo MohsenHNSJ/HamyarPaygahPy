1c3cbf149de486208bb9cd1af6d5a99f2399c16f
Mohsen <97420292+MohsenHNSJ@users.noreply.github.com>
2026-02-03 06:12:00 +0000

    Updated list of ignored errors

--- src/hamyar_paygah/new_ui/__init__.py
+++ src/hamyar_paygah/new_ui/__init__.py
@@ -3,6 +3,6 @@
 The following comments should be added to each dialog file individually.
 """
 # pylint: disable=C0114,E0611,W0611,C0115,C0103,R0205,C0116,R0915,C0301,W1406,W0201
-# ruff: noqa: UP009, RUF100, F401, D100, N801, D101, N803, ANN001, UP004, N802, D102, ANN201,UP025,N806,PGH003
+# ruff: noqa: UP009, RUF100, F401, D100, N801, D101, N803, ANN001, UP004, N802, D102, ANN201,UP025,N806,PGH003,PLR0915, E501, Q003
 # mypy: ignore-errors
 # type: ignore[all]
