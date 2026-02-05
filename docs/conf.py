0cb036588f906bceb4596fbe54d12ba1d6db5924
Mohsen <97420292+MohsenHNSJ@users.noreply.github.com>
2026-02-05 11:09:03 +0000

    Added mappings for intersphinx

--- docs/conf.py
+++ docs/conf.py
@@ -95,6 +95,7 @@ extensions += ["sphinx.ext.intersphinx"]
 intersphinx_mapping: dict[str, tuple[str, None]] = {
     "python": ("https://docs.python.org/3/", None),
     "aiohttp": ("https://docs.aiohttp.org/en/stable/", None),
+    "pyside6": ("https://doc.qt.io/qtforpython-6", None),
 }
 
 # Add support for nice Not Found 404 pages
