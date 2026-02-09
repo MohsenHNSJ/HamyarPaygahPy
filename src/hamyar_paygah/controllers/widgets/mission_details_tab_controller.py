6e13bb451273ec476ae1c0cc1cf140336e76fb41
Mohsen <97420292+MohsenHNSJ@users.noreply.github.com>
2026-02-09 18:43:31 +0000

    Initial mission details tab

--- src/hamyar_paygah/controllers/widgets/mission_details_tab_controller.py
+++ src/hamyar_paygah/controllers/widgets/mission_details_tab_controller.py
@@ -1,9 +1,17 @@
 """Controller for mission details tab."""
 
 # pylint: disable=E0611,I1101,R0903
+from typing import TYPE_CHECKING
+
 from PySide6.QtWidgets import QWidget
+from qasync import asyncSlot  # type: ignore[import-untyped]
 
 import hamyar_paygah.new_ui.widgets.mission_details_tab as ui_mdt
+from hamyar_paygah.config.server_config import load_server_address
+from hamyar_paygah.services.mission_details_service import get_mission_details
+
+if TYPE_CHECKING:
+    from hamyar_paygah.models.mission_details_model import MissionDetails
 
 
 class MissionsDetailsTab(QWidget):
@@ -16,3 +24,18 @@ class MissionsDetailsTab(QWidget):
         # Set up the UI
         self.ui = ui_mdt.Ui_mission_details_tab()  # type: ignore[attr-defined]
         self.ui.setupUi(self)
+
+    @asyncSlot()  # type: ignore[untyped-decorator,misc]
+    async def on_search_button_clicked(self) -> None:
+        """Loads the mission details from server and populates the fields."""
+        # Get mission details from server
+        mission_details: MissionDetails = await get_mission_details(
+            str(load_server_address()),
+            int(self.ui.mission_id_line_edit.text()),
+            int(self.ui.patient_id_line_edit.text()),
+        )
+
+        # Populate the name field
+        self.ui.patient_name_field.setText(
+            mission_details.information.patient_name,
+        )
--- src/hamyar_paygah/new_ui/widgets/mission_details_tab.py
+++ src/hamyar_paygah/new_ui/widgets/mission_details_tab.py
@@ -44,7 +44,17 @@ from PySide6.QtGui import (
     QRadialGradient,
     QTransform,
 )
-from PySide6.QtWidgets import QApplication, QLabel, QSizePolicy, QWidget
+from PySide6.QtWidgets import (
+    QApplication,
+    QHBoxLayout,
+    QLabel,
+    QLayout,
+    QLineEdit,
+    QPushButton,
+    QSizePolicy,
+    QVBoxLayout,
+    QWidget,
+)
 
 
 class Ui_mission_details_tab:
@@ -52,14 +62,86 @@ class Ui_mission_details_tab:
         if not mission_details_tab.objectName():
             mission_details_tab.setObjectName("mission_details_tab")
         mission_details_tab.resize(750, 650)
+        sizePolicy = QSizePolicy(
+            QSizePolicy.Policy.Expanding,
+            QSizePolicy.Policy.Expanding,
+        )
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(
+            mission_details_tab.sizePolicy().hasHeightForWidth(),
+        )
+        mission_details_tab.setSizePolicy(sizePolicy)
         mission_details_tab.setMinimumSize(QSize(750, 650))
         mission_details_tab.setLayoutDirection(Qt.LayoutDirection.RightToLeft)
         mission_details_tab.setLocale(QLocale(QLocale.Persian, QLocale.Iran))
-        self.label = QLabel(mission_details_tab)
+        self.verticalLayout_2 = QVBoxLayout(mission_details_tab)
+        self.verticalLayout_2.setObjectName("verticalLayout_2")
+        self.search_parameters = QWidget(mission_details_tab)
+        self.search_parameters.setObjectName("search_parameters")
+        self.search_parameters.setMaximumSize(QSize(16777215, 50))
+        self.horizontalLayout = QHBoxLayout(self.search_parameters)
+        self.horizontalLayout.setObjectName("horizontalLayout")
+        self.mission_id_label = QLabel(self.search_parameters)
+        self.mission_id_label.setObjectName("mission_id_label")
+        self.mission_id_label.setMaximumSize(QSize(16777215, 50))
+
+        self.horizontalLayout.addWidget(self.mission_id_label)
+
+        self.mission_id_line_edit = QLineEdit(self.search_parameters)
+        self.mission_id_line_edit.setObjectName("mission_id_line_edit")
+        self.mission_id_line_edit.setMaximumSize(QSize(16777215, 22))
+
+        self.horizontalLayout.addWidget(self.mission_id_line_edit)
+
+        self.patient_id_label = QLabel(self.search_parameters)
+        self.patient_id_label.setObjectName("patient_id_label")
+        self.patient_id_label.setMaximumSize(QSize(16777215, 22))
+
+        self.horizontalLayout.addWidget(self.patient_id_label)
+
+        self.patient_id_line_edit = QLineEdit(self.search_parameters)
+        self.patient_id_line_edit.setObjectName("patient_id_line_edit")
+        self.patient_id_line_edit.setMaximumSize(QSize(16777215, 22))
+
+        self.horizontalLayout.addWidget(self.patient_id_line_edit)
+
+        self.search_button = QPushButton(self.search_parameters)
+        self.search_button.setObjectName("search_button")
+        self.search_button.setMaximumSize(QSize(16777215, 22))
+
+        self.horizontalLayout.addWidget(self.search_button)
+
+        self.verticalLayout_2.addWidget(self.search_parameters)
+
+        self.mission_details = QVBoxLayout()
+        self.mission_details.setObjectName("mission_details")
+        self.mission_details.setSizeConstraint(
+            QLayout.SizeConstraint.SetDefaultConstraint,
+        )
+        self.information_section = QWidget(mission_details_tab)
+        self.information_section.setObjectName("information_section")
+        self.information_section.setMaximumSize(QSize(16777215, 100))
+        self.verticalLayout = QVBoxLayout(self.information_section)
+        self.verticalLayout.setObjectName("verticalLayout")
+        self.label = QLabel(self.information_section)
         self.label.setObjectName("label")
-        self.label.setGeometry(QRect(310, 240, 57, 14))
+
+        self.verticalLayout.addWidget(self.label)
+
+        self.patient_name_field = QLineEdit(self.information_section)
+        self.patient_name_field.setObjectName("patient_name_field")
+        self.patient_name_field.setReadOnly(True)
+
+        self.verticalLayout.addWidget(self.patient_name_field)
+
+        self.mission_details.addWidget(self.information_section)
+
+        self.verticalLayout_2.addLayout(self.mission_details)
 
         self.retranslateUi(mission_details_tab)
+
+        self.search_button.setDefault(True)
 
         QMetaObject.connectSlotsByName(mission_details_tab)
 
@@ -73,10 +155,31 @@ class Ui_mission_details_tab:
                 None,
             ),
         )
+        self.mission_id_label.setText(
+            QCoreApplication.translate(
+                "mission_details_tab",
+                "\u0634\u0645\u0627\u0631\u0647 \u0645\u0627\u0645\u0648\u0631\u06cc\u062a",
+                None,
+            ),
+        )
+        self.patient_id_label.setText(
+            QCoreApplication.translate(
+                "mission_details_tab",
+                "\u0634\u0645\u0627\u0631\u0647 \u0645\u062f\u062f\u062c\u0648",
+                None,
+            ),
+        )
+        self.search_button.setText(
+            QCoreApplication.translate(
+                "mission_details_tab",
+                "\u062c\u0633\u062a\u062c\u0648",
+                None,
+            ),
+        )
         self.label.setText(
             QCoreApplication.translate(
                 "mission_details_tab",
-                "TextLabel",
+                "\u0646\u0627\u0645 \u0645\u062f\u062f\u062c\u0648",
                 None,
             ),
         )
